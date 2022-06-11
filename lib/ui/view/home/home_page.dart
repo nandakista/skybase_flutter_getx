a2ef83fd49b7ed6cebb4a3374c628da1c925edf3
nandakista <nanda.kista@gmail.com>
2022-06-11 10:09:35 +0700

    Update HomePage

--- lib/ui/view/home/home_page.dart
+++ lib/ui/view/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:double_back_to_close_app/double_back_to_close_app.dart';
 import 'package:flutter/material.dart';
 import 'package:varcore_flutter_base/core/themes/app_style.dart';
 
@@ -8,13 +9,26 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Text('Home Page', style: AppStyle.subtitle4),
+      body: SafeArea(
+        child: DoubleBackToCloseApp(
+          snackBar: SnackBar(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            behavior: SnackBarBehavior.floating,
+            margin: const EdgeInsets.symmetric(vertical: 100, horizontal: 40),
+            content: const Text("Tap sekali lagi untuk keluar.",
+                textAlign: TextAlign.center),
           ),
-        ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Text('Home Page', style: AppStyle.subtitle4),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
