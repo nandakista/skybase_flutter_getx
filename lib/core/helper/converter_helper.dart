a7bad04d2dcefca2820354752d9a29f7e288004f
nandakista <nanda.kista@gmail.com>
2022-06-30 10:19:21 +0700

    Update String converted helper to Extension

--- lib/core/helper/converter_helper.dart
+++ lib/core/helper/converter_helper.dart
@@ -27,41 +27,47 @@ class AppConverter {
     }
   }
 
-  static String snakeCaseToNormal(String input) {
-    return input
-        .replaceAll('_', ' ')
-        .toLowerCase()
-        .split(' ')
+  static String replaceCharAt(String oldString, int index, String newChar) {
+    return oldString.substring(0, index) +
+        newChar +
+        oldString.substring(index + 1);
+  }
+}
+
+extension StringExtension on String {
+  String capitalize() {
+    return split(' ')
         .map((e) => '${e[0].toUpperCase()}${e.substring(1)}')
         .join(' ');
   }
 
-  static String toSnakeCase(String input) {
-    return input
-        .replaceAll(' ', '_')
+  String toSnakeCase() {
+    return replaceAll(' ', '_')
         .toLowerCase()
         .split(' ')
         .map((e) => '${e[0].toLowerCase()}${e.substring(1)}')
         .join(' ');
   }
 
-  static String replaceCharAt(String oldString, int index, String newChar) {
-    return oldString.substring(0, index) +
-        newChar +
-        oldString.substring(index + 1);
-  }
-
-  static String toIDR({required int amount}) {
-    return NumberFormat.currency(locale: 'id', symbol: 'Rp', decimalDigits: 0)
-        .format(amount);
+  String snakeToNormalCase() {
+    return replaceAll('_', ' ')
+        .toLowerCase()
+        .split(' ')
+        .map((e) => '${e[0]}${e.substring(1)}')
+        .join(' ');
   }
 
   /// The required format string is "Rp 0.000..."
   ///
   /// the return is [Int]
-  static int fromIDR({required String amount}) {
-    final newString = amount.substring(3, amount.length);
-    final newAmount = newString.replaceAll('.', '');
-    return int.tryParse(newAmount) ?? 0;
+  int fromIDR() {
+    return int.tryParse(substring(3, length).replaceAll('.', '')) ?? 0;
+  }
+}
+
+extension IntExtension on int {
+  String toIDR() {
+    return NumberFormat.currency(locale: 'id', symbol: 'Rp ', decimalDigits: 0)
+        .format(this);
   }
 }
