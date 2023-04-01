f84f181413b097e00e8862a911b55ac0531d9cfe
nanda.kista <nanda.kista@gmail.com>
2023-04-02 06:11:47 +0700

    Update list extension

--- lib/core/extension/list_extension.dart
+++ lib/core/extension/list_extension.dart
@@ -38,3 +38,19 @@ extension ListNullExtension<T> on List<T>? {
     return null;
   }
 }
+
+extension Iterables<E> on Iterable<E> {
+  Map<K, List<E>> groupBy<K>(K Function(E) keyFunction) => fold(
+      <K, List<E>>{},
+          (Map<K, List<E>> map, E element) =>
+      map..putIfAbsent(keyFunction(element), () => <E>[]).add(element));
+}
+
+extension Unique<E, Id> on List<E> {
+  List<E> unique([Id Function(E element)? id, bool inplace = true]) {
+    final ids = <dynamic>{};
+    var list = inplace ? this : List<E>.from(this);
+    list.retainWhere((x) => ids.add(id != null ? id(x) : x as Id));
+    return list;
+  }
+}
