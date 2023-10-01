f3d98e9196f16f9d054ab310a4567f85ec72e1bc
nanda.kista <nanda.kista@gmail.com>
2023-10-01 10:16:08 +0700

    .

---
+++ lib/ui/widgets/permission_denied_view.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'sky_button.dart';
+
+class PermissionDeniedView extends StatelessWidget {
+  const PermissionDeniedView({
+    Key? key,
+    required this.title,
+    required this.message,
+    required this.onRequest,
+    this.buttonTitle,
+  }) : super(key: key);
+
+  final String title;
+  final String message;
+  final String? buttonTitle;
+  final VoidCallback onRequest;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 12),
+            Text(
+              message,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 24),
+            SkyButton(
+              text: buttonTitle ?? 'txt_request_permission'.tr,
+              onPressed: onRequest,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
