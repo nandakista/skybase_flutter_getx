4d81b2be5b694088812618c836ad3b9815ba6999
nanda.kista <nanda.kista@gmail.com>
2024-06-27 11:57:58 +0700

    Update new gradle structure

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -2,15 +2,14 @@ import './main.dart' as main_app;
 import 'config/environment/app_env.dart';
 import 'config/environment/config_data.dart';
 import 'config/network/api_token_manager.dart';
+import 'dev/dev_token.dart';
 
 void main() async {
-  /// TODO: To test this repository you can change this clientToken to Github token
-  /// You can get github token with : Profile > Settings > Developer Settings > Personal access tokens
   AppEnv.set(
     environment: Environment.PRODUCTION,
     configuration: ConfigData(
       baseUrl: 'https://api.github.production.com',
-      clientToken: 'Some Client Token',
+      clientToken: gitToken,
       tokenType: TokenType.ACCESS_TOKEN,
     ),
   );
